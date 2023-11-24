cd2596c46ac228c844bb1885fed8384599ebb469
gospodi25 <150433966+gospodi25@users.noreply.github.com>
2023-11-24 03:26:16 +0300

    Update solar_vis.py

--- solar_vis.py
+++ solar_vis.py
@@ -54,7 +54,7 @@ def scale_y(y):
     **y** — y-координата модели.
     """
 
-    return int(y*scale_factor) + window_width//2
+    return int(y*scale_factor) + window_height//2
 
 
 def create_star_image(space, star):
@@ -84,7 +84,7 @@ def create_planet_image(space, planet):
     x = scale_x(planet.x)
     y = scale_y(planet.y)
     r = planet.R
-    star.image = space.create_oval([x - r, y - r], [x + r, y + r], fill=planet.color)
+    planet.image = space.create_oval([x - r, y - r], [x + r, y + r], fill=planet.color)
 
 
 def update_system_name(space, system_name):
